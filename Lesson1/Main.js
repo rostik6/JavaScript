3a2f03196800e4a8407fbec0f82c7643303d7716
rostik6 <rostikglovac@gmail.com>
2022-11-01 18:09:50 +0200

    Lesson1

---
+++ Lesson1/Main.js
@@ -0,0 +1,45 @@
+"use strict";
+let hello ='hello'
+let owu ='owu'
+let com ='com'
+let ua ='ua'
+let number1 ='1'
+let number2 ='10'
+let number3='-999'
+let number4 ='123'
+let number5 ='3.14'
+let number6 ='2.7'
+let number7='16'
+let arc ='true'
+let arca ='false'
+const PI = 3.14;
+
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(number1)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+console.log(number5)
+console.log(number6)
+console.log(number7)
+console.log(arc)
+console.log(arca)
+
+
+let firstName = 'Rostyslav';
+let middleName = 'Vasylovich';
+let lastName = 'Hlovach';
+
+let person = `${lastName} ${firstName} ${middleName}`;
+
+
+console.log(person)
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
